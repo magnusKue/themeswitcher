5e94d9dc792d2c428a1b70c1640efafa41dce9b7
magnusKue <magnusku@gmx.de>
2024-11-27 16:10:12 +0100

    made paths in restore script relative

--- restore_after_reboot.sh
+++ restore_after_reboot.sh
@@ -2,6 +2,8 @@ wal -R
 
 selected_path=$(cat $HOME/.cache/wal/wal)
 
-sudo python $HOME/code/system/rofi-wallpath-changer.py /home/Magnus/.cache/wal/style-5.rasi $selected_path
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-sudo python $HOME/code/system/hyprlock-wallpath-changer.py /home/Magnus/.config/hypr/hyprlock.conf $selected_path
+sudo python $script_dir/src/themeswitcher/rofi-wallpath-changer.py /home/Magnus/.cache/wal/style-5.rasi $selected_path
+
+sudo python $script_dir/src/themeswitcher/hyprlock-wallpath-changer.py /home/Magnus/.config/hypr/hyprlock.conf $selected_path
